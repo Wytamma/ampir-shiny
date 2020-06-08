01e1f7cc69c1ef6ec7cd2d8333ffc65dd649da3b
Wytamma <wytamma.wirth@me.com>
2020-06-08 07:10:06 +0000

    Add README

---
+++ README.md
@@ -0,0 +1,4 @@
+# ampir-shiny
+
+Shiny app wrapping [Ampir](https://github.com/Legana/ampir).
+
--- app.R
+++ app.R
@@ -16,29 +16,29 @@ library(data.table)
 fileName <- 'little_test.fasta'
 
 ui <- fluidPage(
-    textAreaInput(
-      "caption", 
-      "Enter FASTA", 
-      readChar(fileName, file.info(fileName)$size),
-      height = "250px") %>%
-      shiny::tagAppendAttributes(style = 'width: 100%;'),
-    DT::dataTableOutput("value")
+  textAreaInput(
+    "caption",
+    "Enter FASTA",
+    readChar(fileName, file.info(fileName)$size),
+    height = "250px"
+  ) %>%
+    shiny::tagAppendAttributes(style = 'width: 100%;'),
+  DT::dataTableOutput("value")
 )
 
 server <- function(input, output) {
-  
   output$value <- DT::renderDataTable({
     fil <- tempfile(fileext = ".data")
     writeLines(input$caption, fil)
     my_protein_df <- read_faa(fil)
     print(my_protein_df)
-    my_prediction <- predict_amps(my_protein_df, model = "precursor")
+    my_prediction <-
+      predict_amps(my_protein_df, model = "precursor")
     setcolorder(my_prediction, c("seq_name", "prob_AMP", "seq_aa"))
-    DT::datatable(my_prediction, options = list(
-        scrollX = TRUE,
-        scrollCollapse = TRUE
-      ))
-    })
+    DT::datatable(my_prediction,
+                  options = list(scrollX = TRUE,
+                                 scrollCollapse = TRUE))
+  })
 }
 
 # Run the application
